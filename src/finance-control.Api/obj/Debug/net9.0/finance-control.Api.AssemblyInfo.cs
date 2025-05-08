19fa7661d67e1abf39bf85f80422521b4ef72ed6
Mateus-de-Moura <mateus.demoura@hotmail.com>
2025-05-08 18:23:34 -0300

    Normalize line endings

--- src/finance-control.Api/obj/Debug/net9.0/finance-control.Api.AssemblyInfo.cs
+++ src/finance-control.Api/obj/Debug/net9.0/finance-control.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("finance-control.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b51ec68bb671739b8db87ced24b90bfd8f14ef0")]
 [assembly: System.Reflection.AssemblyProductAttribute("finance-control.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("finance-control.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
