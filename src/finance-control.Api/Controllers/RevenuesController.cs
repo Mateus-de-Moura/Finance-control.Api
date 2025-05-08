7555a967d4f60187aec93debd8e6a16b776fbb9b
Mateus-de-Moura <mateus.demoura@hotmail.com>
2025-05-08 18:21:05 -0300

    Feat: adjusting gitgnore

---
+++ src/finance-control.Api/Controllers/RevenuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace finance_control.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RevenuesController : ControllerBase
+    {
+    }
+}
