221d4494ca90827737eeb501f28cddc18dd684bd
Mateus-de-Moura <mateus.demoura@hotmail.com>
2025-05-19 20:31:51 -0300

    Feat: adicionando  update de fotos de usuários

---
+++ src/finance-control.Application/UserCQ/Commands/UpdatePhotoUserCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using finance_control.Application.Response;
+using finance_control.Domain.Entity;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace finance_control.Application.UserCQ.Commands
+{
+    public class UpdatePhotoUserCommand : IRequest<ResponseBase<User>>
+    {
+        public string EmailUser { get; set; }
+        public IFormFile Photo {  get; set; }
+    }
+}
