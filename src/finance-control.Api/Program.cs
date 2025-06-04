97d666fb84800639ed5b138dd855df75477c6356
Mateus-de-Moura <mateus.demoura@hotmail.com>
2025-06-04 14:38:19 -0300

    fix: ajuste cors

--- src/finance-control.Api/Program.cs
+++ src/finance-control.Api/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("CorsPolicy", policy =>
     {
         policy
-          .WithOrigins("http://localhost:5173") // porta React
+          .WithOrigins(["http://localhost:5173", "http://192.168.18.5:8085/"]) 
           .AllowAnyHeader()
           .AllowAnyMethod()
           .AllowCredentials();
