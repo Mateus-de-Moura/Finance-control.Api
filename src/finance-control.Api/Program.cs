3a67bc5e799a1d453d4e6546225929425212bb08
Mateus-de-Moura <mateus.msantos@grupovamos.com.br>
2025-09-26 10:47:22 -0300

    fix adjustings

--- src/finance-control.Api/Program.cs
+++ src/finance-control.Api/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:8085")
+        policy.WithOrigins("http://localhost:8085", "http://192.168.18.198:8085")
                .AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials();
